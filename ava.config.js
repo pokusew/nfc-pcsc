9c35b9215e3d28dc09e3fa44b09da67e2099d6f9
Martin Endler <pokusew@seznam.cz>
2024-12-23 11:14:34 +0100

    Upgrade devDependencies
    Update AVA config (AVA upgrade from v5 to v6).

--- ava.config.js
+++ ava.config.js
@@ -20,26 +20,28 @@ module.exports = {
 	files: [
 		'./test/**/*',
 	],
-	ignoredByWatcher: [
-		//
-		// some files and directories are ignored by default,
-		// see https://github.com/avajs/ava/blob/main/docs/recipes/watch-mode.md#ignoring-changes
-		//
-		// note:
-		//   AVA dependency tracking (https://github.com/avajs/ava/blob/main/docs/recipes/watch-mode.md#dependency-tracking)
-		//   currently does not work with native ES modules.
-		//   See
-		//     https://github.com/avajs/ava/issues/2388
-		//     https://github.com/avajs/ava/pull/3123
-		//     https://github.com/avajs/ava/issues/2905
-		//
-		// use the following to debug:
-		//   DEBUG=ava:watcher npx ava --watch
-		//   or
-		//   DEBUG=ava:* npx ava --watch
-		//
-		'./.idea/',
-		'./temp/',
-		'./dist/',
-	],
+	watchMode: {
+		ignoreChanges: [
+			//
+			// some files and directories are ignored by default,
+			// see https://github.com/avajs/ava/blob/main/docs/recipes/watch-mode.md#ignoring-changes
+			//
+			// note:
+			//   AVA dependency tracking (https://github.com/avajs/ava/blob/main/docs/recipes/watch-mode.md#dependency-tracking)
+			//   currently does not seem to work. However, since AVA v6 it should work.
+			//   See
+			//     https://github.com/avajs/ava/issues/2388
+			//     https://github.com/avajs/ava/pull/3123
+			//     https://github.com/avajs/ava/issues/2905
+			//
+			// use the following to debug:
+			//   DEBUG=ava:watcher npx ava --watch
+			//   or
+			//   DEBUG=ava:* npx ava --watch
+			//
+			'./.idea/',
+			'./temp/',
+			'./dist/',
+		],
+	},
 };
